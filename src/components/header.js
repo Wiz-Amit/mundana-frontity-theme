15e8eb4d150c42273e4249cb0ab4480d60bfd4ab
Amit Hazra <ahazratwt@gmail.com>
2021-05-14 12:42:46 +0530

    removed old title and description

--- src/components/header.js
+++ src/components/header.js
@@ -7,10 +7,6 @@ const Header = ({ state }) => {
   return (
     <>
       <Container>
-        <StyledLink link="/">
-          <Title>{state.frontity.title}</Title>
-        </StyledLink>
-        <Description>{state.frontity.description}</Description>
         <MobileMenu />
       </Container>
       <Nav />
@@ -22,26 +18,13 @@ const Header = ({ state }) => {
 export default connect(Header);
 
 const Container = styled.div`
-  width: 848px;
-  max-width: 100%;
-  box-sizing: border-box;
-  padding: 24px;
-  color: #fff;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
 `;
 
 const Title = styled.h2`
-  margin: 0;
-  margin-bottom: 16px;
 `;
 
 const Description = styled.h4`
-  margin: 0;
-  color: rgba(255, 255, 255, 0.7);
 `;
 
 const StyledLink = styled(Link)`
-  text-decoration: none;
 `;
